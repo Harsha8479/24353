d6a79c564007a28408d76e32a1962656264cd17b
Harsha8479 <146656579+Harsha8479@users.noreply.github.com>
2025-07-09 16:49:37 +0530

    Convert all TypeScript files to JavaScript

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./src/**/*.{jsx,tsx}", "./*.html"],
+    content: ["./src/**/*.{jsx,js}", "./*.html"],
     theme: {
         extend: {
             colors: {
